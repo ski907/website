2475de72de9ed8cb479395bf0b5eb76db68509c3
ski907 <chandler.engel@gmail.com>
2025-12-23 00:20:28 -0500

    troubleshooting js

--- sparkline.js
+++ sparkline.js
@@ -1,13 +1,9 @@
 const canvas = document.getElementById("tempSpark");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 220;
-canvas.height = 40;
-
-// draw a test line
 ctx.strokeStyle = "red";
 ctx.lineWidth = 2;
 ctx.beginPath();
-ctx.moveTo(0, 20);
-ctx.lineTo(220, 20);
+ctx.moveTo(0, canvas.height / 2);
+ctx.lineTo(canvas.width, canvas.height / 2);
 ctx.stroke();
